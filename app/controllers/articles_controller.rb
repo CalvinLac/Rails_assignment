b452c0e33ba994e4ac35210d46eb8a879d43c207
Calvin <Calvin.lacc@gmail.com>
2016-08-02 01:13:26 -0600

    Implemented the function to edit your articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    @article = Article.all
   end
 
   def new
@@ -20,6 +20,17 @@ class ArticlesController < ApplicationController
   def show
    @article = Article.find(params[:id])
 
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update ({:title => params[:title]})
+    @article.update ({:body => params[:body]})
+    redirect_to article_path(@article)
   end
 
 
